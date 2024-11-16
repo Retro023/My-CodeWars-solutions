8b1447c9aedd91f57b2a6e41938fb20e3907c884
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-16 21:24:00 +0000

    Add files via upload

---
+++ python/Convert_a_Boolean_to_a_String.py
@@ -0,0 +1,6 @@
+def boolean_to_string(b):
+    if b == True:
+        b = "True"
+    else:
+        b = "False"
+    return b
---
+++ python/DNA_to_RNA_Conversion.py
@@ -0,0 +1,5 @@
+def dna_to_rna(dna):
+    rna = dna.replace('T', 'U')
+    return rna
+
+# we use the replace() method to replace the T and U in the dna
