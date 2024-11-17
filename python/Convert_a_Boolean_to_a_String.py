f1d488b47d731e43a1dccc5d4fded6b15c2c636c
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-17 00:09:22 +0000

    Delete python directory

--- python/Convert_a_Boolean_to_a_String.py
+++
@@ -1,6 +0,0 @@
-def boolean_to_string(b):
-    if b == True:
-        b = "True"
-    else:
-        b = "False"
-    return b
--- python/DNA_to_RNA_Conversion.py
+++
@@ -1,5 +0,0 @@
-def dna_to_rna(dna):
-    rna = dna.replace('T', 'U')
-    return rna
-
-# we use the replace() method to replace the T and U in the dna
