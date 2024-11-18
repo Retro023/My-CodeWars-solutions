6cd6bb3b08e1446e68edf5c0b40bbb56fc4427c4
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-18 19:48:26 +0000

    Create  You_only_need_one-Beginner.py

---
+++ python/You_only_need_one-Beginner.py
@@ -0,0 +1,8 @@
+def check(seq, elem):
+    if elem in seq:
+        return True
+    else:
+        return False
+    
+    
+#We use a if statement to check if the element is in the sequence if it is we return True if not we Return False
