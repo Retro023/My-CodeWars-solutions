617f1dee909710385a3b53a56dc373860b988824
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-19 19:36:05 +0000

    Create Convert_a_string_to_an_array.py

---
+++ python/Convert_a_string_to_an_array.py
@@ -0,0 +1,6 @@
+def string_to_array(s):
+    split_string = s.split(" ")
+    return split_string
+
+# we split the input string s inot a list of words using the split() method as a delimiter,
+#Then we store the result into a variable called split_string which we then return
