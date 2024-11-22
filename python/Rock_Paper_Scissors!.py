b337525f5716f5a65537841b3b9abe45a99bcd15
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-22 16:10:43 +0000

    Update Rock_Paper_Scissors!.py

--- python/Rock_Paper_Scissors!.py
+++ python/Rock_Paper_Scissors!.py
@@ -16,3 +16,14 @@ def rps(p1, p2):
     
 #We use a if statement block to check the users choices with eachother and return the winner based on rules of,
 #the game however if both inputs are the same it will result in a draw
+
+
+#####TASK#######
+
+#Rock Paper Scissors
+#Let's play! You have to return which player won! In case of a draw return Draw!.
+
+#Examples(Input1, Input2 --> Output):
+#"scissors", "paper" --> "Player 1 won!"
+#"scissors", "rock" --> "Player 2 won!"
+#"paper", "paper" --> "Draw!"
