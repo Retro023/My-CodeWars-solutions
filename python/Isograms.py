445155c583fd22ec04a9b545f196c9931b84cbbf
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-22 16:04:14 +0000

    Update Isograms.py

--- python/Isograms.py
+++ python/Isograms.py
@@ -8,3 +8,16 @@ def is_isogram(string):
 # we then compare that using ==  with the length of the string using the len() method again and if the length,
 #of the unique string is the same length of the origanl string it must be a istogram we then save it as a varible,
 # then return that variable.
+
+
+
+
+#######TASK######
+#An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+#Implement a function that determines whether a string that contains only letters is an isogram. 
+#Assume the empty string is an isogram. Ignore letter case.
+
+#Example: (Input --> Output)
+#"Dermatoglyphics" --> true
+#"aba" --> false
+#"moOse" --> false (ignore letter case)
