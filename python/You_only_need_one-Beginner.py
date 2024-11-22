2da3963ea1deb8f774461c91b90c4c660c12a1c7
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-22 16:08:38 +0000

    Update You_only_need_one-Beginner.py

--- python/You_only_need_one-Beginner.py
+++ python/You_only_need_one-Beginner.py
@@ -6,3 +6,12 @@ def check(seq, elem):
     
     
 #We use a if statement to check if the element is in the sequence if it is we return True if not we Return False
+
+
+
+
+####TASK####
+
+#You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+#Array can contain numbers or strings. X can be either.
+#Return true if the array contains the value, false if not.
