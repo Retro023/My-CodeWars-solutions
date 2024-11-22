502b1cb10c0da9da2f3d08359e89f40b0d3150f5
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-22 16:11:54 +0000

    Update Sum_without_highest_and_lowest_number.py

--- python/Sum_without_highest_and_lowest_number.py
+++ python/Sum_without_highest_and_lowest_number.py
@@ -11,3 +11,20 @@ def sum_array(arr):
     #Then we sort the array highest to lowest and then save it to a variable called sorted_arr after which we use the,
     #sum() method to work out the summary and remove the smallest and largest element finally we save it to a,
     #Variable summary and return that variable
+
+
+
+####TASK######
+
+#Task
+#Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+#The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+#Mind the input validation.
+
+#Example
+#{ 6, 2, 1, 8, 10 } => 16
+#{ 1, 1, 11, 2, 3 } => 6
+#Input validation
+#If an empty value ( null, None, Nothing, nil etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
