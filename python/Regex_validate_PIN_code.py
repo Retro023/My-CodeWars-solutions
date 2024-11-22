020aeb5280fc98fa025059e0f510f84ab3d129ea
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-22 16:06:02 +0000

    Update Regex_validate_PIN_code.py

--- python/Regex_validate_PIN_code.py
+++ python/Regex_validate_PIN_code.py
@@ -7,3 +7,16 @@ def validate_pin(pin):
 # we check the length of the pin  using the len() method to make sure the pin is either 4 or 6 chars long,
 #Then we use the isdigit() method to verify that the chars are all digits which if they all are digits it,
 #returns True if the pin is nither it returns False
+
+
+
+
+#####TASK#####
+
+##ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+#If the function is passed a valid PIN string, return true, else return false.
+
+#Examples (Input --> Output)
+#"1234"   -->  true
+#"12345"  -->  false
+#"a234"   -->  false
