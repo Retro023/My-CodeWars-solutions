733b94d01e84a3d87e380e430a7754024f7d5f6e
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-22 16:32:39 +0000

    Update MakeUpperCase.py

--- python/MakeUpperCase.py
+++ python/MakeUpperCase.py
@@ -3,3 +3,8 @@ def make_upper_case(s):
     return upper_s
 #uses the .upper() method to change s variable so it has captital letters it saves it as the variable,
 # upper_s so its easy to read and understand
+
+
+
+#####TASK#####
+#Write a function which converts the input string to uppercase.
