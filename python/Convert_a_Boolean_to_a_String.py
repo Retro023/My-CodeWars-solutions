5c51cfb622d813b48c200804b0e33a1d52537ae6
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-22 16:23:44 +0000

    Update Convert_a_Boolean_to_a_String.py

--- python/Convert_a_Boolean_to_a_String.py
+++ python/Convert_a_Boolean_to_a_String.py
@@ -6,3 +6,11 @@ def boolean_to_string(b):
     return b
 
 #We use a if statment to check the boolean value of the variable b then we return it as a string value
+
+
+
+
+#####TASK#####
+
+#Description:
+#Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
