0d1fd228d612b5af1b48d3680ebca95ae5ebb2cc
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-22 16:22:03 +0000

    Update ReturningStrings.py

--- python/ReturningStrings.py
+++ python/ReturningStrings.py
@@ -3,3 +3,11 @@ def greet(name):
     return greeting
 
 #we use a variable called greeting to store a formated string value that replaces the {name} with whatever the name is
+
+
+
+####TASK#####
+#Make a function that will return a greeting statement that uses an input; 
+#your program should return, "Hello, <name> how are you doing today?".
+
+#[Make sure you type the exact thing I wrote or the program may not execute properly]
