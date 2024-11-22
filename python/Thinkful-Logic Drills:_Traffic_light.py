2f579d647a8a731025c619dd811104fc1625eeab
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-22 16:00:39 +0000

    Update Thinkful-Logic Drills:_Traffic_light.py

--- python/Thinkful-Logic Drills:_Traffic_light.py
+++ python/Thinkful-Logic Drills:_Traffic_light.py
@@ -6,17 +6,16 @@ def update_light(current):
     elif current == "red":
         return "green"
     
-
-        
-        
-        
-        
-########TASK########        
-        
 # We use a if elif statment to check the current colour of the traffic light we then return the correct colour based,
 #on how a traffic light works so green > yellow > red > green etc
 
 
+
+
+        
+        
+########TASK########        
+        
 #You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
 
 #Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
