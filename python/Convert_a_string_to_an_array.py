4e245c409b80ab2b4cbf76c9c071c286637ba152
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-22 16:07:23 +0000

    Update Convert_a_string_to_an_array.py

--- python/Convert_a_string_to_an_array.py
+++ python/Convert_a_string_to_an_array.py
@@ -4,3 +4,15 @@ def string_to_array(s):
 
 # we split the input string s inot a list of words using the split() method as a delimiter,
 #Then we store the result into a variable called split_string which we then return
+
+
+
+
+
+####TASK#####
+#Write a function to split a string and convert it into an array of words.
+
+#Examples (Input ==> Output):
+#"Robin Singh" ==> ["Robin", "Singh"]
+
+#"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
