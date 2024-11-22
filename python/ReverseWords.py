c5d2daff8ebad183884fa48bc19daae58cc2e149
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-22 16:16:50 +0000

    Update ReverseWords.py

--- python/ReverseWords.py
+++ python/ReverseWords.py
@@ -4,3 +4,16 @@ def reverse_words(text):
 
 # we  first split each word by space then we reverse each word and then we join it back with spaces beifre finally,
 #saving the output to a variable called reversed and then returning that value
+
+
+
+
+
+
+#####TASK#####
+#Complete the function that accepts a string parameter, 
+#and reverses each word in the string. All spaces in the string should be retained.
+
+#Examples
+#"This is an example!" ==> "sihT si na !elpmaxe"
+#"double  spaces"      ==> "elbuod  secaps"
