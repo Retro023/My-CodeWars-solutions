a0e40610bd8c4d57e1f31f1b9034a6bf5df5684f
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-11-23 23:15:33 +0000

    Create  Beginner_Series_#4_Cockroach.py

---
+++ python/Beginner_Series_#4_Cockroach.py
@@ -0,0 +1,20 @@
+def cockroach_speed(s):
+     return s * 100000 // 3600
+
+    
+    
+# We use a simple formula s * 100,000 ÷ 3600 then we  return it as an int
+
+
+
+
+
+####TASK#####
+#The cockroach is one of the fastest insects. 
+#Write a function which takes its speed in km per hour and returns it in cm per second, 
+#rounded down to the integer (= floored).
+
+#For example:
+
+#1.08 --> 30
+#Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
