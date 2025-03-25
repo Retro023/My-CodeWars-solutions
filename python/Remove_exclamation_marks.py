261550ed7c626b7c6be50ba5e36b294ef483d228
MuteAvery <107277836+Retro023@users.noreply.github.com>
2025-03-25 12:59:46 +0000

    Create Remove_exclamation_marks.py

---
+++ python/Remove_exclamation_marks.py
@@ -0,0 +1,17 @@
+def remove_exclamation_marks(s):
+    return s.replace('!','')
+
+#We use the replace method to replace  ! with an empty string
+
+
+
+
+
+
+
+
+
+########TASK###########
+#Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+#Fundamentals
+#Strings
