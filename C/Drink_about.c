3446b3d72d64c8a4c7a69375d69379d34d1d05fb
MuteAvery <107277836+Retro023@users.noreply.github.com>
2025-07-24 23:42:08 +0100

    Update Drink_about.c

--- C/Drink_about.c
+++ C/Drink_about.c
@@ -17,4 +17,34 @@ else if (age >= 21){
 }
 else {
    return "age unknown drink coke";
- 
+
+
+
+/*  
+Challenge
+
+    Kids drink toddy.
+    Teens drink coke.
+    Young adults drink beer.
+    Adults drink whisky.
+
+Make a function that receive age, and return what they drink.
+
+Rules:
+    Children under 14 old.
+    Teens under 18 old.
+    Young under 21 old.
+    Adults have 21 or more.
+
+Examples: (Input --> Output)
+
+13 --> "drink toddy"
+17 --> "drink coke"
+18 --> "drink beer"
+20 --> "drink beer"
+30 --> "drink whisky"
+
+
+
+
+*/
