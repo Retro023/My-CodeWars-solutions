143cad225f253dc76037fa1a3d534023d116c758
MuteAvery <107277836+Retro023@users.noreply.github.com>
2025-07-24 23:40:35 +0100

    Add files via upload

---
+++ C/Drink_about.c
@@ -0,0 +1,20 @@
+//    do not allocate memory:
+//    return a string literal
+
+const char *people_with_age_drink(unsigned age) {
+
+if (age < 14){
+  return "drink toddy";
+}
+else if (age < 18){
+  return "drink coke";
+}
+else if (age < 21){
+  return "drink beer";
+}
+else if (age >= 21){
+  return "drink whisky";
+}
+else {
+   return "age unknown drink coke";
+ 
